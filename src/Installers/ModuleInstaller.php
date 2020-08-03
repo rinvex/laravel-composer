c28e36ff363a4c23630297202f0589e359072553
Abdelrahman Omran <me@omranic.com>
2020-08-03 12:00:07 +0400

    Add ability to autoload/unload installed/uninstalled modules

--- src/Installers/ModuleInstaller.php
+++ src/Installers/ModuleInstaller.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Rinvex\Composer\Installers;
 
+use Exception;
 use Composer\Package\PackageInterface;
+use Illuminate\Foundation\Application;
 use Composer\Repository\InstalledRepositoryInterface;
 
 class ModuleInstaller extends LibraryInstaller
@@ -30,6 +32,8 @@ class ModuleInstaller extends LibraryInstaller
      */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
+        $this->loadModule($package, true);
+
         parent::install($repo, $package);
     }
 
@@ -38,6 +42,64 @@ class ModuleInstaller extends LibraryInstaller
      */
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
+        $this->loadModule($package, false);
+
         parent::uninstall($repo, $package);
+    }
+
+    /**
+     * Autoload installed module.
+     *
+     * @param \Composer\Package\PackageInterface $package
+     * @param bool                               $status
+     *
+     * @throws \Exception
+     *
+     * @return void
+     */
+    protected function loadModule(PackageInterface $package, bool $status): void
+    {
+        $laravel = new Application(getcwd());
+        $modulesManifestPath = $laravel->bootstrapPath('cache'.DIRECTORY_SEPARATOR.'modules.php');
+
+        if ($status) {
+            $modulesManifest = file_exists($modulesManifestPath) ? array_merge(require $modulesManifestPath, [$package->getPrettyName() => ['active' => false, 'autoload' => true]]) : [];
+        } else {
+            $modulesManifest = file_exists($modulesManifestPath) ? require $modulesManifestPath : [];
+            unset($modulesManifest[$package->getPrettyName()]);
+        }
+
+        if (! is_writable($dirname = dirname($modulesManifestPath))) {
+            throw new Exception("The {$dirname} directory must be present and writable.");
+        }
+
+        $this->replaceFile(
+            $modulesManifestPath, '<?php return '.var_export($modulesManifest, true).';'
+        );
+    }
+
+    /**
+     * Write the contents of a file, replacing it atomically if it already exists.
+     *
+     * @param  string  $path
+     * @param  string  $content
+     *
+     * @return void
+     */
+    protected function replaceFile($path, $content)
+    {
+        // If the path already exists and is a symlink, get the real path...
+        clearstatcache(true, $path);
+
+        $path = realpath($path) ?: $path;
+
+        $tempPath = tempnam(dirname($path), basename($path));
+
+        // Fix permissions of tempPath because `tempnam()` creates it with permissions set to 0600...
+        chmod($tempPath, 0777 - umask());
+
+        file_put_contents($tempPath, $content);
+
+        rename($tempPath, $path);
     }
 }
