315d303c61bc390e4031be017d5f590ec3b9c2fb
Abdelrahman Omran <me@omranic.com>
2020-12-22 02:53:21 +0400

    Upgrade to Laravel v8 & composer v2

--- src/Plugins/ModulePlugin.php
+++ src/Plugins/ModulePlugin.php
@@ -12,12 +12,43 @@ use Rinvex\Composer\Installers\ModuleInstaller;
 class ModulePlugin implements PluginInterface
 {
     /**
-     * {@inheritdoc}
+     * Apply plugin modifications to Composer
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
      */
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new ModuleInstaller($io, $composer);
 
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // @TODO
+    }
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // @TODO
     }
 }
